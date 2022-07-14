c6ff062329ac7f826320794820a3824c7713803c
Arcaderic <109065191+Arcaderic@users.noreply.github.com>
2022-07-14 11:07:54 +0200

    Created enemy that follows 'Amogus'

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Amogus":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . 2 2 2 2 2 2 2 2 2 . . . . . 
+. . 2 2 2 f f f 2 2 2 . . . . . 
+. . 2 2 f f 1 f f 2 2 . . . . . 
+. . 2 2 f 1 f 1 f 2 2 . . . . . 
+. . 2 2 f f 1 f f 2 2 . . . . . 
+. . 2 2 2 f f f 2 2 2 . . . . . 
+. . 2 f 1 2 2 2 1 f 2 . . . . . 
+. . 2 2 f 1 f 1 f 2 2 . . . . . 
+. . 2 1 2 f 1 f 2 1 2 . . . . . 
+. . 2 1 2 2 2 2 2 1 2 . . . . . 
+. . 2 1 2 . . . 2 1 2 . . . . . 
+. . 2 1 2 . . . 2 1 2 . . . . . 
+. . 2 2 2 . . . 2 2 2 . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
